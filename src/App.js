a16bd22b14e896960b172c5341d11178e71dc9d3
rafik gasmi <gasmi.rafik.rg@gmail.com>
2021-11-11 16:48:49 +0100

    rafik 2 commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Rafik from "./Rafik"
 function App() {
   return (
     <div className="App">
@@ -8,15 +8,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <Rafik />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
       </header>
     </div>
   );
